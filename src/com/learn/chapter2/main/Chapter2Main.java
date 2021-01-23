10fba8c5078a357edfc30e5bcbd8895815c077ea
chenweiqiang <chenweiqiang@lightinthebox.com>
2021-01-23 16:52:18 +0800

    Mybatis(SqlSessionFactory SqlSession Mapper) without-maven min-runnable demo

---
+++ src/com/learn/chapter2/main/Chapter2Main.java
@@ -0,0 +1,29 @@
+package com.learn.chapter2.main;
+
+import com.learn.chapter2.mapper.RoleMapper;
+import com.learn.chapter2.po.Role;
+import com.learn.chapter2.util.SqlSessionFactoryUtil;
+import org.apache.ibatis.session.SqlSession;
+
+public class Chapter2Main {
+    public static void main(String[] args) {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = SqlSessionFactoryUtil.openSqlSession();
+            RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+            Role role = new Role();
+            role.setRoleName("testName");
+            role.setNote("testNote");
+            roleMapper.insertRole(role);
+            roleMapper.deleteRole(1L);
+            sqlSession.commit();
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+            sqlSession.rollback();
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+}
