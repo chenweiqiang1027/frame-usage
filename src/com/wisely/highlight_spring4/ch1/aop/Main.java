dc9cf3a1178c6a7cc725ea51208752f1d671605d
chenweiqiang <chenweiqiang@lightinthebox.com>
2021-07-04 16:03:05 +0800

    di

---
+++ src/com/wisely/highlight_spring4/ch1/aop/Main.java
@@ -0,0 +1,14 @@
+package com.wisely.highlight_spring4.ch1.aop;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AopConfig.class);
+//        DemoAnnotationService demoAnnotationService = context.getBean(DemoAnnotationService.class);
+//        demoAnnotationService.add();
+        DemoMethodService demoMethodService = context.getBean(DemoMethodService.class);
+        demoMethodService.add();
+        context.close();
+    }
+}
